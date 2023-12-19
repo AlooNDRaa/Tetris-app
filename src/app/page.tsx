b9b57351b3bc1ca2e9bfadb61630ae39d45dc459
Gadea Alondra <alonngadea@gmail.com>
2023-12-19 19:41:19 -0300

    Sufering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.scss'
+import Perfil from '@/components/perfil/perfil'
 
 export default function Home() {
   return (
-    <div className={styles.div}>
-        <h1>
-          hola
-        </h1>
+    <div className={styles.div2}>
+        <Perfil/>
     </div>
   )
 }
---
+++ src/components/perfil/perfil.tsx
@@ -0,0 +1,9 @@
+import stylescomp1 from './perfil.module.scss'
+
+export default function Perfil() {
+    return <header className={stylescomp1.head}>
+        <h1 className={stylescomp1.h1a}>
+            Hola
+        </h1>
+    </header>;
+  }
